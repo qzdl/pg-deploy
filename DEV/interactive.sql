e2c4a3208f9e1fd56886df9904e5089eb42eaae6
Samuel Culpepper <samuel.culpepper@thinkproject.com>
2020-06-05 17:07:53 +0200

    Expose column type, defaults, constraints
    this implementation adds some complexity, but enables the full type info and associated catalog information for a column to be ripped from pg_catalog pretty seamlessly.
    
    add create table full implementation in notes.org

--- DEV/interactive.sql
+++ DEV/interactive.sql
@@ -21,10 +21,9 @@ DROP / RECREATE FUNCTION AT  %
 ', (select current_timestamp);
 
     -- NOTE: can this be done programmatically?
-    drop function if exists public.reconsile_desired(
-         og_schema_name character varying, ds_schema_name character varying, object_name character varying);
+    drop function if exists public.reconsile_desired(og_schema_name text, ds_schema_name text, object_name text);
 
-    select into func (select file.read(rdir || 'function.sql'));
+    select into func (select file.read(rdir||'function.sql'));
 
 raise notice '%', func;
 
@@ -47,6 +46,87 @@ drop table if exists testr.a;
 drop table if exists testp.a;
 
 create table testr.a(i int, ii text, iii bit);
-create table testp.a(ii text, iii bit, iv text);
+create table testp.a(ii text, iv text);
 
-select public.reconsile_desired('testr', 'testp', 'a')
+select public.reconsile_desired('testr', 'testp', 'a');
+
+
+--
+
+  -- with signs as (
+  --               select 'DROP' as sign,
+  --                      r_source.column_name as col
+  --               from information_schema.columns as r_source
+  --               where table_name = 'a'  -- to yield d0
+  --                 and table_schema = 'testr'
+  --                 and not exists (
+  --                   select column_name
+  --                   from information_schema.columns as r_target
+  --                   where r_target.table_name = 'a'
+  --                     and r_target.table_schema = 'testp'
+  --                     and r_source.column_name = r_target.column_name) -- AJ predicate
+  --               union -- inverse for `ADD'
+  --               select 'ADD' as sign,
+  --                      a_target.column_name as col
+  --               from information_schema.columns as a_target
+  --               where table_name = 'a'       -- to yield d1
+  --                 and table_schema = 'testp'
+  --                 and not exists (
+  --                   select column_name
+  --                   from information_schema.columns as a_source
+  --                   where a_source.table_name = 'a'
+  --                     and a_source.table_schema = 'testr'
+  --                     and a_source.column_name = a_target.column_name) -- AJ predicate
+  --           )
+  --           SELECT
+  --               signs.sign,
+  --               signs.col,
+  --               b.nspname as schema_name,
+  --               b.relname as table_name,
+  --               a.attname as column_name,
+  --               pg_catalog.format_type(a.atttypid, a.atttypmod) as column_type,
+  --               -- defaults; FIXME: maybe throw into CTE to reduce duplication of query logic?
+  --               CASE WHEN
+  --                   (SELECT substring(pg_catalog.pg_get_expr(d.adbin, d.adrelid) for 128)
+  --                    FROM   pg_catalog.pg_attrdef d
+  --                    WHERE  d.adrelid = a.attrelid AND d.adnum = a.attnum AND a.atthasdef)
+  --               IS NOT NULL THEN
+  --                   'DEFAULT '|| (SELECT substring(pg_catalog.pg_get_expr(d.adbin, d.adrelid) for 128)
+  --                                 FROM pg_catalog.pg_attrdef d
+  --                                 WHERE d.adrelid = a.attrelid AND d.adnum = a.attnum AND a.atthasdef)
+  --               ELSE
+  --                   ''
+  --               END as column_default_value,
+  --               CASE WHEN a.attnotnull = true THEN
+  --                   'NOT NULL'
+  --               ELSE
+  --                   'NULL'
+  --               END as column_not_null,
+  --               a.attnum as attnum,
+  --               e.max_attnum as max_attnum
+  --           FROM
+  --               pg_catalog.pg_attribute a
+  --               LEFT JOIN signs ON a.attname = signs.col AND signs.sign = 'ADD'
+  --               INNER JOIN
+  --                (SELECT c.oid,
+  --                   n.nspname,
+  --                   c.relname
+  --                 FROM pg_catalog.pg_class c
+  --                      LEFT JOIN pg_catalog.pg_namespace n ON n.oid = c.relnamespace
+  --                 WHERE c.oid = 33015
+  --                     -- FIXME: visible necessary? these preds should really be applied in the `_tables` query
+  --                     -- AND pg_catalog.pg_table_is_visible(c.oid)
+  --                 ORDER BY 2, 3) b
+  --               ON a.attrelid = b.oid
+  --               INNER JOIN
+  --                (SELECT
+  --                     a.attrelid,
+  --                     max(a.attnum) as max_attnum
+  --                 FROM pg_catalog.pg_attribute a
+  --                 WHERE a.attnum > 0
+  --                   AND NOT a.attisdropped
+  --                 GROUP BY a.attrelid) e
+  --               ON a.attrelid=e.attrelid
+  --           WHERE a.attnum > 0
+  --             AND NOT a.attisdropped
+  --           ORDER BY a.attnum
--- function.sql
+++ function.sql
@@ -1,82 +1,120 @@
 CREATE FUNCTION public.reconsile_desired(
-    og_schema_name character varying,
-    ds_schema_name character varying,
-    object_name character varying)
-    -- FIXME: change `varying' to `name'? (ref existing object in db)
+    source_schema text,
+    target_schema text,
+    object_name text
+)
 RETURNS SETOF text AS
 $BODY$
 DECLARE
-    table_oid text;
-    sign_attr_rec record;
     col_ddl text;
-    col_rec record;
+    _tables record;
+    _columns record;
+    _constraints record;
 BEGIN
-    -- check if og = ds; y-> do nothing
-    --                   n-> calculate diff & barf alter ddl
-   /* IF (1=0) */
-        /* RETURN col_ddl; */
-    /*
-            TODO
-    */
-    -- get name, identifier for ds_obj so catalog info can be grabbed later
-    table_oid := (
-        SELECT c.oid
-        FROM pg_catalog.pg_class c
-            LEFT JOIN pg_catalog.pg_namespace n
-                ON n.oid = c.relnamespace
+    -- restrict to operational tables
+    FOR _tables IN
+        SELECT c.relname, c.oid FROM pg_catalog.pg_class c
+            LEFT JOIN pg_catalog.pg_namespace n ON n.oid = c.relnamespace
         WHERE relkind = 'r'
-            AND n.nspname = ds_schema_name
+            AND (n.nspname = target_schema OR n.nspname = source_schema)
             AND relname~ ('^('||object_name||')$')
-        ORDER BY c.relname);
+        ORDER BY c.relname
+    LOOP -- _tables
+        -- get column info (name, type, NULL, constraints, defaults)
+        FOR _columns IN
+        -- compute diff for source->target
+            with signs as (
+                select 'DROP' as sign,
+                       r_source.column_name as col
+                from information_schema.columns as r_source
+                where table_name = object_name  -- to yield d0
+                  and table_schema = source_schema
+                  and not exists (
+                    select column_name
+                    from information_schema.columns as r_target
+                    where r_target.table_name = object_name
+                      and r_target.table_schema = target_schema
+                      and r_source.column_name = r_target.column_name) -- AJ predicate
+                union -- inverse for `ADD'
+                select 'ADD' as sign,
+                       a_target.column_name as col
+                from information_schema.columns as a_target
+                where table_name = object_name       -- to yield d1
+                  and table_schema = target_schema
+                  and not exists (
+                    select column_name
+                    from information_schema.columns as a_source
+                    where a_source.table_name = object_name
+                      and a_source.table_schema = source_schema
+                      and a_source.column_name = a_target.column_name) -- AJ predicate
+            )
+            SELECT
+                signs.sign,
+                signs.col,
+                b.nspname as schema_name,
+                b.relname as table_name,
+                a.attname as column_name,
+                pg_catalog.format_type(a.atttypid, a.atttypmod) as column_type,
+                -- defaults; FIXME: maybe throw into CTE to reduce duplication of query logic?
+                CASE WHEN
+                    (SELECT substring(pg_catalog.pg_get_expr(d.adbin, d.adrelid) for 128)
+                     FROM   pg_catalog.pg_attrdef d
+                     WHERE  d.adrelid = a.attrelid AND d.adnum = a.attnum AND a.atthasdef)
+                IS NOT NULL THEN
+                    'DEFAULT '|| (SELECT substring(pg_catalog.pg_get_expr(d.adbin, d.adrelid) for 128)
+                                  FROM pg_catalog.pg_attrdef d
+                                  WHERE d.adrelid = a.attrelid AND d.adnum = a.attnum AND a.atthasdef)
+                ELSE
+                    ''
+                END as column_default_value,
+                CASE WHEN a.attnotnull = true THEN
+                    'NOT NULL'
+                ELSE
+                    'NULL'
+                END as column_not_null,
+                a.attnum as attnum,
+                e.max_attnum as max_attnum
+            FROM
+                pg_catalog.pg_attribute a
+                LEFT JOIN signs ON a.attname = signs.col AND signs.sign = 'ADD'
+                INNER JOIN
+                 (SELECT c.oid,
+                    n.nspname,
+                    c.relname
+                  FROM pg_catalog.pg_class c
+                       LEFT JOIN pg_catalog.pg_namespace n ON n.oid = c.relnamespace
+                  WHERE c.oid = _tables.oid
+                      -- FIXME: visible necessary? these preds should really be applied in the `_tables` query
+                      -- AND pg_catalog.pg_table_is_visible(c.oid)
+                  ORDER BY 2, 3) b
+                ON a.attrelid = b.oid
+                INNER JOIN
+                 (SELECT
+                      a.attrelid,
+                      max(a.attnum) as max_attnum
+                  FROM pg_catalog.pg_attribute a
+                  WHERE a.attnum > 0
+                    AND NOT a.attisdropped
+                  GROUP BY a.attrelid) e
+                ON a.attrelid=e.attrelid
+            WHERE a.attnum > 0
+              AND NOT a.attisdropped
+            ORDER BY a.attnum
+        LOOP -- _columns
+            IF _columns.sign = 'DROP' THEN
+                RAISE NOTICE 'DROP for column %', _columns;
+                col_ddl := 'ALTER TABLE '||source_schema||'.'||object_name||' DROP COLUMN '||_columns.col||';';
+            ELSE
+                RAISE NOTICE 'ADD for column %', _columns;
+                SELECT INTO col_ddl
+                'ALTER TABLE '||source_schema||'.'||object_name||' '
+                ||'ADD COLUMN '||_columns.col||' '||_columns.column_type||' '
+                ||_columns.column_default_value||' '||_columns.column_not_null;
+            END IF;
+              RETURN NEXT col_ddl;
+        END LOOP; -- _columns
 
-    FOR sign_attr_rec IN
-        -- compute diff for og->ds
-        select 'DROP' as sign,
-               r_source.column_name as col
-        from information_schema.columns as r_source
-        where table_name = object_name  -- to yield d0
-          and table_schema = og_schema_name
-          and not exists (
-            select column_name
-            from information_schema.columns as r_target
-            where r_target.table_name = object_name
-              and r_target.table_schema = ds_schema_name
-              and r_source.column_name = r_target.column_name) -- AJ predicate
-        union -- inverse for `ADD'
-        select 'ADD' as sign,
-               a_target.column_name as col
-        from information_schema.columns as a_target
-        where table_name = object_name       -- to yield d1
-          and table_schema = ds_schema_name
-          and not exists (
-            select column_name
-            from information_schema.columns as a_source
-            where a_source.table_name = object_name
-              and a_source.table_schema = og_schema_name
-              and a_source.column_name = a_target.column_name) -- AJ predicate
-    LOOP
-        IF sign_attr_rec.sign = 'DROP' THEN
-            col_ddl := 'ALTER TABLE '||og_schema_name||'.'||object_name||' DROP COLUMN '||sign_attr_rec.col||';';
-        ELSE
-            col_ddl := 'ALTER TABLE '||og_schema_name||'.'||object_name||' ADD COLUMN '||sign_attr_rec.col||' int;';
-        END IF;
-    RETURN NEXT col_ddl;
-    END LOOP; -- table_rec
-
-    -- if exists, generate `ALTERS' for DROP
-    /*
-            TODO
-    */
-    -- collect DROP; no further action on drops (constraints, indexes will be truncated automatically)
-
-    -- if exists, generate `ALTERS' for ADD (to include indexes, constraints, defaults from ds_obj)
-    /*
-            TODO
-    */
-    -- collect ADDq
-
-    -- return (sign | expr)
-
+    END LOOP; -- _tables
 END
 $BODY$
     LANGUAGE plpgsql STABLE;
