8439febbb1f30b7535336cc03e239a1b120bac9e
Samuel Culpepper <samuel.culpepper@thinkproject.com>
2020-10-27 13:51:55 +0100

    first working pipeline boy

--- test/schema/new/table.sql
+++ test/schema/new/table.sql
@@ -1,4 +1,4 @@
-CREATE TABLE first_i_was_afraid (
-  a int constraint hmm check (i > 5),
+CREATE TABLE staying_alive.first_i_was_afraid (
+  a int constraint hmm check (a > 5),
   b text
 );
--- test/schema/old/table.sql
+++ test/schema/old/table.sql
@@ -1,3 +1,8 @@
-CREATE TABLE first_i_was_afraid (
-  a int constraint check (i > 5)
+CREATE TABLE staying_alive.first_i_was_afraid (
+  a int check (a > 5)
+);
+
+
+CREATE TABLE staying_alive.i_was_petrified (
+  living_life_without_you_by_my_side bool
 );
