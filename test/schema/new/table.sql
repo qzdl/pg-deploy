9a0d8679be8e0ea70ff1916321870e2b73699712
Samuel Culpepper <samuel.culpepper@thinkproject.com>
2020-10-30 13:36:20 +0100

    add schema repo

--- test/schema/new/table.sql
+++ test/schema/new/table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE staying_alive.first_i_was_afraid (
   a int constraint hmm check (a > 5),
   b text,
-  c bool default 1 constraint yep check (c = 1)
+  c bool default true constraint yep check (c = true)
 );
